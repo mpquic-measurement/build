005764dd972017fff84dd8e6b6eb4a3ba2d8c4d4
clarkzjw <i@jinwei.me>
2023-06-14 21:39:33 +0000

    try porting to ubuntu 22.04

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-sudo docker build -t ns3-dce-base:20.04 -f Dockerfile-base-20.04 .
-sudo docker build -t ns3-dce-fec:20.04 -f Dockerfile-fec-20.04 .
-sudo docker build -t ns3-dce-mpquic:20.04 -f Dockerfile .
+sudo docker build -t ns3-dce-base:22.04 -f Dockerfile-base-22.04 .
+sudo docker build -t ns3-dce-fec:22.04 -f Dockerfile-fec-22.04 .
+sudo docker build -t ns3-dce-mpquic:22.04 -f Dockerfile .
